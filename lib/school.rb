a7b91ca74762ae490df664bbc3ef72f402697085
github username <github email address>
2017-06-08 15:35:36 -0400

    Done.

--- lib/school.rb
+++ lib/school.rb
@@ -1,1 +1,37 @@
+require 'pry-byebug'
 # code here!
+class School
+
+  def roster
+    @roster
+  end
+
+  def initialize(name)
+    @name = name
+    @roster = {}
+  end
+
+
+  def add_student(student, grade)
+    if @roster[grade].class == Array
+      @roster[grade] << student
+    else
+    @roster[grade] = []
+    @roster[grade] << student
+    end
+  end
+
+  def grade(grade)
+    @roster[grade]
+  end
+
+  def sort
+    @roster.each do |grade, student|
+      @roster[grade].sort!
+      # binding.pry
+    end
+  end
+
+end
+
+# Pry.start
